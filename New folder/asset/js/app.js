3ea520d98414d29f527dc5aa867213453f9a1f8d
Ayush <sachanaayush7@gmail.com>
2022-07-04 14:07:51 +0530

    day 22

---
+++ New folder/asset/js/app.js
@@ -0,0 +1,20 @@
+// console.log(1)
+// console.log(2)
+
+
+// this settimeout will delay the function by 5000 milisecond
+// const callback = () => {
+//     console.log("callback function run")
+// }
+// const delay = 5000
+
+// setTimeout(callback,delay)
+
+console.log(1)
+console.log(2)
+setTimeout(() => {
+    console.log(5)
+
+},1000)
+console.log(3)
+console.log(4)
